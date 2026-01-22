6bfaecd08c7e1e69187f0efda3eea2d47c1bd8af
Łukasz Kółko <kolkolukasz@icloud.com>
2026-01-22 21:27:51 +0100

    prettify

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from "@eslint/js";
-import prettierConfig from "eslint-config-prettier/flat";
-import perfectionist from "eslint-plugin-perfectionist";
-import prettierPluginRecommended from "eslint-plugin-prettier/recommended";
-import { defineConfig } from "eslint/config";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import ts from "typescript-eslint";
+import js from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier/flat'
+import perfectionist from 'eslint-plugin-perfectionist'
+import prettierPluginRecommended from 'eslint-plugin-prettier/recommended'
+import { defineConfig } from 'eslint/config'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import ts from 'typescript-eslint'
 
 export default defineConfig([
   js.configs.recommended,
   ts.configs.recommended,
   prettierConfig,
   prettierPluginRecommended,
-  perfectionist.configs["recommended-natural"],
+  perfectionist.configs['recommended-natural'],
   {
     languageOptions: {
-      ecmaVersion: "latest",
+      ecmaVersion: 'latest',
     },
   },
   {
-    extends: ["js/recommended"],
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    extends: ['js/recommended'],
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
     languageOptions: { globals: globals.node },
     plugins: { js },
   },
   tseslint.configs.recommended,
-]);
+])
