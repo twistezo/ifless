101675d6b30d9a4dd6ab3e4616c40e38c828603d
Łukasz Kółko <kolkolukasz@icloud.com>
2026-02-03 21:09:54 +0100

    Fix console.group nesting in examples

--- examples/main.ts
+++ examples/main.ts
@@ -9,41 +9,41 @@ console.log('user.active', user.active)
 console.log('isAdmin', isAdmin)
 console.groupEnd()
 
+console.group('\nClassic:')
 if (user.active && !isAdmin) {
-  console.group('\nClassic:')
   console.log('Expression: (user.active && !isAdmin)')
   console.log('Result: OK')
-  console.groupEnd()
 }
+console.groupEnd()
 
+console.group('\nWith "when":')
 when`${user.active} AND NOT ${isAdmin}`(() => {
-  console.group(`\nWith 'when':`)
   console.log('Expression: when`${user.active} AND NOT ${isAdmin}`')
   console.log('Result: OK')
-  console.groupEnd()
 })
+console.groupEnd()
 
+console.group('\nWith context and aliases:')
 when.ctx({
   admin: isAdmin,
   userActive: user.active,
 })`#userActive AND NOT #admin`(() => {
-  console.group(`\nWith context and aliases:`)
   console.log(
     'Expression: when.ctx({ admin: isAdmin, userActive: user.active, })`#userActive AND NOT #admin`',
   )
   console.log('Result: OK')
-  console.groupEnd()
 })
+console.groupEnd()
 
+console.group('\nComplex example:')
 when.ctx({
   a: true,
   b: false,
   c: false,
 })`(#a AND #b) OR (NOT #c AND ${true})`(() => {
-  console.group(`\nComplex example:`)
   console.log(
     'Expression: when.ctx({ a: true, b: false, c: false, })`(#a AND #b) OR (NOT #c AND ${true})`',
   )
   console.log('Result: OK')
-  console.groupEnd()
 })
+console.groupEnd()
